9b3d94f47d9ae8f2b8d152cfb7297b6ba627c6d3
Jun Shimura <junshimura@gmail.com>
2018-12-22 12:11:05 +0900

    TestScene genarated
    TestScene genarated

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    public float speed = 100.0f;
+
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    Vector3 moveVector=new Vector3();
+    void Update () {
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.z = Input.GetAxis("Vertical");
+        rigidbody.AddForce(moveVector * speed);
+	}
+}
